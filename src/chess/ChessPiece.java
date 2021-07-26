c651ca634afeae798fc8b6707f1e0b3ae9a55aa2
R4f43lA94r3c1d0 <rafaelaparecido.oliveirasilva@gmail.com>
2021-07-26 14:02:34 -0300

    Piece move count

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -7,6 +7,7 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece
 {
     private Color color;
+    private int moveCount;
 
     //Custom constructor (With arguments)
     public ChessPiece(Board board, Color color)
@@ -21,7 +22,22 @@ public abstract class ChessPiece extends Piece
         return color;
     }
 
+    public int getMoveCount()
+    {
+        return moveCount;
+    }
+
     //Methods
+    public void increaseMoveCount()
+    {
+        moveCount++;
+    }
+
+    public void decreaseMoveCount()
+    {
+        moveCount--;
+    }
+
     public ChessPosition getChessPosition ()
     {
         return ChessPosition.fromPosition(position);
